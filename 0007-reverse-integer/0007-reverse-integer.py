c3b1c0a4be28564b62527c09c61c71a86c9cebed
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-05-19 21:57:15 +0530

    Time: 27 ms (17.54%), Space: 13.4 MB (55.71%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -6,7 +6,7 @@ class Solution(object):
         while x:
             digit = x % 10
             reverse = reverse * 10 + digit
-            x //= 10
+            x /= 10
         result = sign * reverse
         if result > 2 ** 31 - 1 or result < -(2 ** 31):
             return 0
