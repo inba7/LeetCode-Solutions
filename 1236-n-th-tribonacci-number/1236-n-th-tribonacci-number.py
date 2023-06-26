4797c894afa624ce0ad82e68e9dcbbb4648a155c
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-06-26 23:50:14 +0530

    Time: 14 ms (72.48%), Space: 13.4 MB (35.97%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -2,7 +2,7 @@ class Solution(object):
     def tribonacci(self, n):
         if n == 0: 
             return 0
-        elif (n == 1 or n == 2):
+        elif (n == 1 or n ==2):
             return 1
         else:
             trib = 0
