b04b29be518e8b41bc564fa270edfd2e29880ada
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-06-27 00:18:40 +0530

    Time: 65 ms (5.07%), Space: 13.7 MB (12.78%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
-        one = 0
-        two = 0
-        for i in reversed(cost):
-            temp = min(i + one, i + two)
-            two = one
-            one = temp
-        return min(one, two)
+        self.table = [-1]*(len(cost)+1)
+        self.table[0]=0
+        self.table[1]=cost[0]
+        for i in range(2,len(cost)+1):
+            self.table[i] = cost[i-1]+min(self.table[i-1],self.table[i-2])
+        print(self.table)
+        return min(self.table[len(cost)],self.table[len(cost)-1])
