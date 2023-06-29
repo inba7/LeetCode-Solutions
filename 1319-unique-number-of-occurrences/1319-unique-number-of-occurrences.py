6009f4454d6f2517c981fe82c0afcf4f6e1465fc
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-06-29 14:41:26 +0530

    Time: 29 ms (30.09%), Space: 13.3 MB (86.64%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def uniqueOccurrences(self, arr):
-        Set, Count = set(arr), []
-        for i in Set:
-            Count.append(arr.count(i))
-        if sorted(list(set(Count))) == sorted(Count):
-            return True
-        else:
-            return False
+        counts=[]
+        set_arr=set(arr)
+        for x in set_arr:
+            if arr.count(x) in counts:
+                return False
+            else:
+                counts.append(arr.count(x))
+        return True
