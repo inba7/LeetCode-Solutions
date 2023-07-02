53ff6e62e21a3daa744604e66df08e423bcab398
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-07-02 22:17:27 +0530

    Time: 186 ms (97.02%), Space: 20.8 MB (79.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,12 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        zeroes = 0
-        full_product = 1
-        for idx, i in enumerate(nums):
-            if i == 0:
-                zeroes += 1
-                if zeroes > 1:
-                    return [0] * len(nums)
-                position = idx
-            else:
-                full_product *= i
-        if zeroes:
-            result = [0] * len(nums)
-            result[position] = full_product
-        else:
-            result = [full_product//i for i in nums]      
-        return result
+        res = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):  
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
