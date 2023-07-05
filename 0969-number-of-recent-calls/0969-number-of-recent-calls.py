67068ebb140238d12f389ebe8afdfab40871d1e3
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-07-05 21:49:27 +0530

    Time: 246 ms (87.33%), Space: 18.4 MB (50.08%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -1,8 +1,9 @@
 class RecentCounter(object):
     def __init__(self):
-        self.q = deque()   
+        self.queue = deque()
+
     def ping(self, t):
-        self.q.append(t)
-        while t - self.q[0] > 3000:
-            self.q.popleft()
-        return len(self.q)
+        self.queue.append(t)
+        while t - 3000 > self.queue[0]:
+            self.queue.popleft()
+        return len(self.queue)
