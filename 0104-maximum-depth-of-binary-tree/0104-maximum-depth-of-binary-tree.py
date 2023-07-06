2a2a24a9822688dd88333dfd3647b4e9d803bd40
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-07-06 21:04:11 +0530

    Time: 33 ms (23.37%), Space: 16.1 MB (54.23%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,9 @@
+def max_depth(root):
+    if not root:
+        return 0
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return max(left_depth, right_depth) + 1
+class Solution(object):
+    def maxDepth(self, root):
+        return max_depth(root)
