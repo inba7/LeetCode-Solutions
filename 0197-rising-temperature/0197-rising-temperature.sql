99de4e19dde58831bbd7bd339172a5a286ef52fe
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-07-13 23:21:09 +0530

    Time: 690 ms (95.67%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,3 @@
-SELECT W1.id FROM Weather W1, Weather W2
-WHERE DATEDIFF(W1.recordDate, W2.recordDate) = 1
-AND W1.temperature > W2.temperature;
+SELECT W1.id FROM Weather W1 INNER JOIN Weather W2
+ON ADDDATE(W1.recordDate, -1) = W2.recordDate
+WHERE W1.temperature > W2.temperature;
