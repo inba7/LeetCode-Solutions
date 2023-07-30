9012624391196ed2d25772bfe261acfa07ebff9c
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-07-30 16:21:15 +0530

    Time: 1334 ms (30.61%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,10 +1,10 @@
-SELECT DISTINCT employee_id, department_id
+SELECT employee_id, department_id
 FROM Employee
-WHERE employee_id IN (
-    SELECT employee_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(*) = 1
-  )
-  OR primary_flag = 'Y'
-ORDER BY employee_id;
+WHERE primary_flag = 'Y'
+OR (employee_id, department_id) IN (SELECT employee_id, department_id
+  FROM Employee
+  WHERE employee_id IN 
+  (SELECT employee_id
+  FROM Employee
+  GROUP BY employee_id
+  HAVING COUNT(department_id) = 1))
