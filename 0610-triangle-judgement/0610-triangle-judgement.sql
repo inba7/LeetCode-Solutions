647c3714bdc6a88357c59aa57901b4e4efde421b
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-07-31 16:27:46 +0530

    Time: 498 ms (85.41%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,2 +1,2 @@
-SELECT x, y, z, CASE WHEN (x+y) > z and (x+z) > y and (y+z) > x THEN 'Yes'
+SELECT *, CASE WHEN (x+y) > z and (x+z) > y and (y+z) > x THEN 'Yes'
 ELSE 'No' END AS triangle FROM Triangle;
