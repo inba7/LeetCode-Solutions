3316448a1ee7eabae01698c0b27748af75738ce7
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-01 22:44:08 +0530

    Time: 630 ms (40.25%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,2 +1,3 @@
 SELECT M.name FROM Employee E INNER JOIN Employee M
-ON E.managerId = M.id GROUP BY E.managerId HAVING COUNT(E.id) >= 5;
+ON E.managerId = M.id WHERE E.managerId IS NOT NULL
+GROUP BY E.managerId HAVING COUNT(E.managerId) >= 5
