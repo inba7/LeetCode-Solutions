2777879b9f6324300a7101c93fa35057d691ba5b
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-11 07:38:36 +0530

    Time: 35 ms (97.33%), Space: 13.5 MB (94.67%) - LeetHub

--- 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.py
+++ 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.py
@@ -2,6 +2,7 @@ class Solution(object):
     def decompressRLElist(self, nums):
         Out = []
         for i in range(0, len(nums), 2):
-            for j in range(nums[i]):
-                Out.append(nums[i+1])
+            Freq, Val = nums[i], nums[i+1]
+            Temp = [Val] * Freq
+            Out.extend(Temp)
         return Out
