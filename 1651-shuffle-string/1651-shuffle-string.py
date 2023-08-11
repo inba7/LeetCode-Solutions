0537d8ed5274942700342cd8c598c59d701c8b5a
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-11 19:55:16 +0530

    Time: 40 ms (53.72%), Space: 13.2 MB (89.22%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.py
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -2,7 +2,4 @@ class Solution(object):
     def restoreString(self, s, indices):
         if indices == sorted(indices):
             return s
-        S = list(s)
-        ZP = zip(indices, S)
-        Z = [val for key, val in sorted(ZP)]
-        return "".join(Z)
+        return ''.join([Val for Key, Val in sorted(zip(indices, s))])
