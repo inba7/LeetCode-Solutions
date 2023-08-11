a06e5dd39e62de903884df0cefb33b2421c2affc
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-11 19:21:32 +0530

    Time: 14 ms (82.89%), Space: 13.4 MB (36.36%) - LeetHub

--- 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.py
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def xorOperation(self, n, start):
         XOR = 0
-        while n > 0:
-            XOR ^= start
-            start += 2
-            n -= 1
+        for num in range(0,n):
+            XOR ^= start + (num*2)
         return XOR
