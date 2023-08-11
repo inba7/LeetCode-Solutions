6b8428090d9b3d347e96ea30c9d82712fdbbc549
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-11 18:37:00 +0530

    Time: 48 ms (93.35%), Space: 13.4 MB (6.3%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.py
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -3,5 +3,5 @@ class Solution(object):
         Count = 0
         for i in range(1,n+1):
             if ((i%3==0) or (i%5==0) or (i%7==0)):
-                Count += i
+                Count+=i
         return Count
