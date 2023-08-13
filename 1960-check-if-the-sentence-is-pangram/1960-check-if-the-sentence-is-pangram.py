a2ee8a694aa42e4768c3ac238468331790bf67d1
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-13 15:27:43 +0530

    Time: 10 ms (94.23%), Space: 13.1 MB (98.27%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,8 +1,4 @@
 class Solution(object):
     def checkIfPangram(self, sentence):
-        Flag = True
-        for x in range(97, 123):
-            if chr(x) not in sentence:
-                Flag = False
-                break
-        return Flag
+        Sentence = set(sentence)
+        return True if len(Sentence) >= 26 else False
