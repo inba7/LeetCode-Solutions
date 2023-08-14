e17ce6df380415ee0a3da9ddc07d5e3ff30c78de
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-14 19:45:12 +0530

    Time: 22 ms (34.97%), Space: 13.3 MB (58.9%) - LeetHub

--- 2401-count-asterisks/2401-count-asterisks.py
+++ 2401-count-asterisks/2401-count-asterisks.py
@@ -1,9 +1,3 @@
 class Solution(object):
     def countAsterisks(self, s):
-        Flag, Count = 0, 0
-        for Char in s:
-            if Char == "*" and Flag%2 == 0:
-                Count += 1
-            elif Char == "|":
-                Flag += 1
-        return Count
+        return sum([Word.count('*') for Word in s.split('|')][0::2])
