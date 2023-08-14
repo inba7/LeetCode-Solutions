23782c7930b646f6055d0d90bd970b97913ae549
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-14 17:24:59 +0530

    Time: 87 ms (97.67%), Space: 13.5 MB (87.91%) - LeetHub

--- 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
@@ -1,7 +1,9 @@
 class Solution(object):
     def makeSmallestPalindrome(self, s):
-        Res = ""
-        mid = len(s)/2
-        for i in range(0, mid):
-            Res += min(s[i], s[len(s)-i-1])
-        return Res + s[mid] + Res[::-1] if len(s)%2==1 else Res + Res[::-1]
+        s = list(s)
+        for x in range(len(s) // 2):
+            if s[x] < s[~x]:
+                s[~x] = s[x]
+            else:
+                s[x] = s[~x]
+        return ''.join(s) 
