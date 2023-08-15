447941b438eaff51eee0f51fdcd03bda9c51291d
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-15 14:02:32 +0530

    Time: 1361 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,7 @@
 SELECT 
-   (SELECT DISTINCT Salary FROM Employee
-    ORDER BY Salary DESC LIMIT 1 OFFSET 1)
+   (SELECT DISTINCT Salary 
+   FROM Employee
+   ORDER BY Salary DESC 
+   LIMIT 1 
+   OFFSET 1)
 AS SecondHighestSalary
