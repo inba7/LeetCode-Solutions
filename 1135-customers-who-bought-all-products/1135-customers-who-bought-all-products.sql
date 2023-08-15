c6c482e7cc632f731009d439f0286ee566b980c7
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-15 19:04:31 +0530

    Time: 1182 ms (42.66%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,2 +1,3 @@
-SELECT customer_id FROM Customer GROUP BY customer_id HAVING 
-COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
+SELECT customer_id FROM Customer
+Group by customer_id
+HAVING COUNT(distinct product_key)=(SELECT COUNT(product_key) FROM Product)
