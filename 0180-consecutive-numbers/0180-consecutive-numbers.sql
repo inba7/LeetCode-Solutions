606accc9af7f8868ad6674fe796b3d319f265bcc
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-16 23:05:30 +0530

    Time: 1459 ms (15.34%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,6 +1,3 @@
-WITH Temp AS
-(SELECT num, lead(num,1) over() num1,
-lead(num,2) over() num2 FROM logs)
-
-SELECT DISTINCT num ConsecutiveNums FROM Temp 
-WHERE (num=num1) AND (num=num2)
+SELECT DISTINCT Num AS ConsecutiveNums FROM Logs WHERE 
+(Id + 1, Num) IN (SELECT * FROM Logs) AND 
+(Id + 2, Num) IN (SELECT * FROM Logs);
