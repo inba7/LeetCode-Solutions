25f3686c302a129520bd539f2bbf84a0ba4b4adb
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-17 19:10:53 +0530

    Time: 1633 ms (79.14%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,6 +1,5 @@
 SELECT person_name FROM 
-(
-  SELECT person_name, turn,
+( SELECT person_name, turn,
   SUM(weight) OVER (ORDER BY turn) AS Total
   FROM Queue
 ) A WHERE Total <= 1000 ORDER BY turn DESC LIMIT 1
