2f232e7bd29da4a023d03c53c186785b089b8419
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-17 19:10:30 +0530

    Time: 1915 ms (55.89%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,3 +1,6 @@
-SELECT q1.person_name FROM Queue q1 LEFT JOIN Queue q2 ON q1.turn >= q2.turn
-GROUP BY q1.turn HAVING SUM(q2.weight) <= 1000 ORDER BY SUM(q2.weight) DESC
-LIMIT 1
+SELECT person_name FROM 
+(
+  SELECT person_name, turn,
+  SUM(weight) OVER (ORDER BY turn) AS Total
+  FROM Queue
+) A WHERE Total <= 1000 ORDER BY turn DESC LIMIT 1
