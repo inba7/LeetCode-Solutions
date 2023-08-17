4d21aa69758002867f0b801d0a3e1629d4947449
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-17 18:38:11 +0530

    Time: 2539 ms (27.61%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,7 +1,10 @@
 WITH Temp AS(
-SELECT D.Name AS Department, E.Name AS Employee, E.Salary AS Salary,
+SELECT D.Name AS Department, 
+E.Name AS Employee, 
+E.Salary AS Salary,
 dense_rank() OVER (partition by D.Name ORDER BY E.Salary DESC) AS Ranking
-FROM Employee E LEFT JOIN Department D ON E.DepartmentId = D.Id) 
+FROM Employee E 
+LEFT JOIN Department D ON E.DepartmentId = D.Id) 
 
 SELECT Department, Employee, Salary
 FROM Temp WHERE Ranking <=3
