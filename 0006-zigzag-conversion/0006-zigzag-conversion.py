9115825dbcb03c6db3b8c911c60e296ba862c7e7
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-18 23:05:38 +0530

    Time: 34 ms (81.06%), Space: 13.4 MB (75.02%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def convert(self, s, numRows):
-        if numRows == 1 or numRows >= len(s):
+        if numRows == 1:
             return s
-        L = [""]*numRows
-        Index, Step = 0, 1
-        for Char in s:
-            L[Index] += Char
-            if Index == 0:
-                Step = 1
-            elif Index == numRows - 1:
-                Step = -1
-            Index += Step
-        return "".join(L)
+        p = 2 * (numRows - 1)
+        result = [""] * numRows
+        for i in xrange(len(s)):
+            floor = i % p
+            if floor >= p//2:
+                floor = p - floor
+            result[floor] += s[i]
+        return "".join(result)
