b6e5ed243195a044cd19a1b77f744a650b3cc03f
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-18 14:35:21 +0530

    Time: 40 ms (90.48%), Space: 13.3 MB (89.28%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,21 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        Head = ListNode()
-        Current = Head
+        Dummy = ListNode(0)
+        Cur = Dummy
         Carry = 0
-        while (l1 != None or l2 != None or Carry != 0):
-            l1_value = l1.val if l1 else 0
-            l2_value = l2.val if l2 else 0
-            Total = l1_value + l2_value + Carry
-            Current.next = ListNode(Total % 10)
-            Carry = Total // 10
-            # Move list pointers forward
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            Current = Current.next
-        return Head.next
+        while l1 or l2:
+            Sum = Carry
+            if l1:
+                Sum += l1.val
+                l1 = l1.next
+            if l2:
+                Sum += l2.val
+                l2 = l2.next
+            Carry = Sum // 10
+            Cur.next = ListNode(Sum % 10)
+            Cur = Cur.next
+        if Carry:
+            Cur.next = ListNode(Carry) 
+        return Dummy.next
