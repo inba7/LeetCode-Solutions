40759cdc94bbb563eeee257479cdef24273ca17b
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-21 16:24:22 +0530

    Time: 14 ms (99.48%), Space: 13.3 MB (46.18%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,21 +1,13 @@
 class Solution(object):
     def romanToInt(self, s):
-        sum = 0
-        index=0
-        dict1={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        dict2={"IV":4,"IX":9,"XL":40,"XC":90,"CD":400,"CM":900}
-        liste=[]
-        for i in s:
-            liste.append(i)
-        
-        while len(liste) > index:
-            if index < len(liste) - 1 and liste[index] + liste[index+1] in dict2:
-                value = dict2[liste[index] + liste[index+1]]
-                sum += value
-                index += 2
+        Roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+        'C': 100, 'D': 500, 'M': 1000}
+        Total, Prev = 0, 0 
+        for char in s:
+            Current = Roman[char]
+            if Current > Prev:
+                Total += Current-2*Prev
             else:
-                value=dict1[liste[index]]
-                sum+=value
-                index += 1
-        
-        return sum
+                Total += Current
+            Prev = Current
+        return Total
