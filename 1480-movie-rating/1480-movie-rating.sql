a6defe0eee7b704bb98b15438ec8ef4548af5b88
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-08-22 14:13:07 +0530

    Time: 2930 ms (42.81%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,9 +1,10 @@
-SELECT DISTINCT FIRST_VALUE(u.name) OVER(ORDER BY COUNT(r.movie_id) DESC, 
-u.name ASC) AS results FROM Users AS u LEFT JOIN MovieRating AS r
-ON u.user_id=r.user_id GROUP BY u.user_id
+(SELECT U.name AS results FROM MovieRating MR INNER JOIN users U
+ON MR.user_id = U.user_id GROUP BY MR.user_id
+ORDER BY COUNT(movie_id) DESC, U.name LIMIT 1)
+
 UNION ALL
-SELECT DISTINCT FIRST_VALUE(m.title) OVER(ORDER BY AVG(r.rating) DESC, 
-m.title ASC) AS results FROM Movies AS m join MovieRating AS r
-ON m.movie_id=r.movie_id 
-WHERE r.created_at BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY m.movie_id
+
+(SELECT M.title AS results FROM MovieRating MR INNER JOIN movies M
+ON MR.movie_id = M.movie_id
+WHERE MONTH(MR.created_at)=02 AND YEAR(MR.created_at)=2020
+GROUP BY MR.movie_id ORDER BY AVG(MR.rating) DESC, M.title LIMIT 1)
