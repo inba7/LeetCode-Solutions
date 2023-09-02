c589ede13229ab8560d437e0e14034817cbb4069
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-02 20:22:22 +0530

    Time: 190 ms (64.52%), Space: 14 MB (12.9%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minExtraChar(self, s, dictionary):
+        cache = {}
+        def dfs(text):
+            if len(text) <= 0:
+                return 0
+            if len(text) in cache:
+                return cache[len(text)]
+            minimum = len(text)
+            for word in dictionary:
+                if text == word:
+                    minimum = 0
+                    cache[len(text)] = minimum
+                    return minimum
+                elif text[:len(word)] == word:
+                    result = dfs(text[len(word):])
+                    minimum = min(minimum, result)
+            minimum = min(minimum, 1 + dfs(text[1:]))
+            cache[len(text)] = minimum
+            return minimum
+        return dfs(s)
