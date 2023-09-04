d2e9c4e4b439d0a629fd54ddef1d2821a908e65c
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-04 16:54:43 +0530

    Time: 18 ms (40.27%), Space: 13.3 MB (30.5%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        dp = [[-1] * n for _ in range(m)]
+        def solve(i, j):
+            if i == m - 1 or j == n - 1:
+                return 1
+            if dp[i][j] != -1:
+                return dp[i][j]
+            dp[i][j] = solve(i + 1, j) + solve(i, j + 1)
+            return dp[i][j]
+
+        return solve(0, 0)
