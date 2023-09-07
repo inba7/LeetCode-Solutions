c794b19b7cde3a83f51f0abecc3790bdbc96f1a2
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-07 21:43:10 +0530

    Time: 15 ms (63.77%), Space: 13.6 MB (34.99%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,52 +1,22 @@
 class Solution(object):
     def reverseBetween(self, head, left, right):
-        if not head or not head.next:
-            return head
-        if left == right:
-            return head
-        curr = head
-        t = None
-        prev = None
-        s = None
-        v = None
-        f = False
-        if left == 1:
-            f = True
-            t = curr
-        s1 = 1
-        while curr:
-            if s1 == left and not f:
-                v = prev
-                t = curr
-            if s1 == right:
-                s = curr.next
-                curr.next = None
-                break
-            prev = curr
-            curr = curr.next
-            s1 += 1
-        if not f:
-            v.next = None
-        ans = self.reverseList(t)
-        t.next = s
-        if not f:
-            v.next = ans
-        if f:
-            head = ans
-        return head
-    def reverseList(self, head):
-        if not head or not head.next:
-            return head
-
-        curr = head
-        prev = None
-        next_node = None
-
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-
-        return prev
-        
+        Dummy = ListNode(0,head)
+        K = right-left+1
+        Prev, i = None, 1
+        Curr = Dummy.next
+        PrevCur = Dummy
+        while i<left:
+            PrevCur = PrevCur.next
+            Curr = Curr.next
+            i+=1
+        NewTail = PrevCur.next
+        Count = 0
+        while K:
+            next = Curr.next
+            Curr.next = Prev
+            Prev = Curr
+            Curr = next
+            K-=1
+        PrevCur.next = Prev
+        NewTail.next = Curr
+        return Dummy.next
