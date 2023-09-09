c955652c7a177259a6284a2a02cbf25bb8f7c7ed
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-09 20:58:48 +0530

    Time: 12 ms (98.37%), Space: 13.3 MB (38.41%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def combinationSum4(self, nums, target):
-        DP = [0] * (target + 1)
+        nums.sort()
+        DP = [0]*(target+1)
         DP[0] = 1
-        for i in range(1, target + 1):
+        for i in range(1,target+1):
             for num in nums:
-                if i - num >= 0:
-                    DP[i] += DP[i - num]        
-        return DP[target]
+                if num > i:
+                    break
+                DP[i] += DP[i-num]
+        return DP[-1]
