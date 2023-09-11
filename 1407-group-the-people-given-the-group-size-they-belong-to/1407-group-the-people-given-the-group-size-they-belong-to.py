3dfe4f4b8c7beb382c876b57b9448fb9ae81c666
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-11 18:36:59 +0530

    Time: 44 ms (87.12%), Space: 13.6 MB (8.33%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupThePeople(self, groupSizes):
+        group_map = {}
+        for i, size in enumerate(groupSizes):
+            if size in group_map:
+                group_map[size].append(i)
+            else:
+                group_map[size] = [i]
+        result = []
+        for size, group in group_map.items():
+            for i in range(0, len(group), size):
+                result.append(group[i:i+size])
+        return result
