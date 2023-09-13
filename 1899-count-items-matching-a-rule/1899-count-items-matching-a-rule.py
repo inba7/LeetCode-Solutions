d3ba749057b2698b7ed9181ae7111f6a5e1735e4
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-13 23:50:46 +0530

    Time: 185 ms (49.41%), Space: 20.9 MB (40%) - LeetHub

--- 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.py
@@ -2,12 +2,12 @@ class Solution(object):
     def countMatches(self, items, ruleKey, ruleValue):
         Count = 0
         if ruleKey == "type":
-            ruleKey = 0
+            rk = 0
         elif ruleKey == "color":
-            ruleKey = 1
+            rk = 1
         elif ruleKey == "name":
-            ruleKey = 2
+            rk = 2
         for item in items:
-            if item[ruleKey] == ruleValue:
+            if item[rk] == ruleValue:
                 Count += 1
         return Count
