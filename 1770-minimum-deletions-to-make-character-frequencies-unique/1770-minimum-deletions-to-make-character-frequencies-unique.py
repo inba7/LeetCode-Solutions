617598483eacea3f40ccad4e5da782a84a4823bb
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-13 14:29:32 +0530

    Time: 180 ms (63.64%), Space: 14.2 MB (87.5%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.py
@@ -2,14 +2,14 @@ class Solution(object):
     def minDeletions(self, s):
         Freq = [0] * 26
         for char in s:
-            Freq[ord(char) - ord('a')] += 1
+            Freq[ord(char)-ord('a')] += 1
         Freq.sort()
         Count = 0
         for i in range(24, -1, -1):
             if Freq[i] == 0:
                 break
-            if Freq[i] >= Freq[i + 1]:
+            if Freq[i] >= Freq[i+1]:
                 Prev = Freq[i]
-                Freq[i] = max(0, Freq[i + 1] - 1)
+                Freq[i] = max(0, Freq[i+1]-1)
                 Count += Prev - Freq[i]
         return Count
