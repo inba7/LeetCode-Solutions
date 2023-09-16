1d26020e6d933eef3c903aa3a21d3655664ddeb7
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-16 21:57:27 +0530

    Time: 35 ms (91.06%), Space: 13.8 MB (87.65%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def canVisitAllRooms(self, Rooms):
-        Seen = [False] * len(Rooms)
-        Seen[0] = True
-        Q = deque([0])
-        while Q:
-            Node = Q.popleft()
-            for Room in Rooms[Node]:
-                if not Seen[Room]:
-                    Seen[Room] = True
-                    Q.append(Room)
-        return all(Seen)
+        Visit  = set()
+        Stack = [0]
+        while Stack: 
+            Room = Stack.pop() 
+            Visit.add(Room)
+            for Key in Rooms[Room]:
+                if Key not in Visit:
+                    Stack.append(Key)
+        return len(Visit) == len(Rooms)
