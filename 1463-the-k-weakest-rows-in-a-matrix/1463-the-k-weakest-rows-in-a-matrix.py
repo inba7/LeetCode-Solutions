9be7299b3cbdd3cacc2f56ca9a1d318d0aef9923
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-18 14:28:09 +0530

    Time: 82 ms (33.33%), Space: 13.3 MB (98.12%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.py
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def kWeakestRows(self, mat, k):
-        Res = [x for x in range(len(mat))]
-        Count = []
-        for row in mat:
-            Count.append(row.count(1))
-        ZP = zip(Count, Res)
-        Zip = [row for count, row in sorted(ZP)]
-        return Zip[:k]
+        m = range(len(mat))
+        rows = sorted(m, key=lambda i: mat[i])
+        del rows[k:]
+        return rows
