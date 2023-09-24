2e440ec9a19b0a2467b265aea28093f74aeeaf1f
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-24 14:40:08 +0530

    Time: 95 ms (64.3%), Space: 13.2 MB (100%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,8 +1,20 @@
 class Solution(object):
-    def minTaps(self, n, ranges):
-        dp=[0]+[n+2]*n
-        for i,x in enumerate(ranges):
-            for j in range(max(i-x+1,0),min(i+x,n)+1):
-                dp[j]=min(dp[j],dp[max(0,i-x)]+1)
+    def minTaps(self, n, R):
+        Tap, Count, Best = 0, 0, -1
+        while Tap < n:
+            Start = max(Tap-100,0)
+            End = min(Tap+100,n+1)
 
-        return dp[n] if dp[n]<n+2 else -1  
+            for i in range(Start,Tap):
+                if R[i]!= 0 and R[i]+i >= Tap and R[i]+i > Best:
+                    Best = R[i] + i
+            for i in range(Tap,End):
+                if R[i] != 0 and i-R[i] <= Tap and i+R[i] > Best:
+                    Best = R[i] + i
+            if Best == -1 or Tap == Best:
+                return -1
+            print(Tap,Best)
+            Count += 1
+            Tap = Best
+        
+        return Count
