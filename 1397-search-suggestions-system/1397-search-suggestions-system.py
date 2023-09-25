ddc0c9cc73fa89f39a339b4fa7e0a1407f04e35d
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-26 00:41:32 +0530

    Time: 52 ms (81.82%), Space: 15.3 MB (69.96%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.py
+++ 1397-search-suggestions-system/1397-search-suggestions-system.py
@@ -1,10 +1,19 @@
 class Solution(object):
     def suggestedProducts(self, products, searchWord):
         products.sort()
-        n=len(products)
-        indices=[i for i in range(n)]
-        res=[]
-        for idx, c in enumerate(searchWord):
-            indices = [i for i in indices if len(products[i])>idx and products[i][idx] == c]                                                                
-            res.append(products[i] for i in indices[:3])        
-        return res 
+        res = []
+        left = 0
+        right = len(products) - 1
+        
+        for i in range(len(searchWord)):
+            ch = searchWord[i]
+            while left <= right and (len(products[left]) <= i or products[left][i] != ch):
+                left += 1
+            while left <= right and (len(products[right]) <= i or products[right][i] != ch):
+                right -= 1
+            
+            if left > right:
+                res.append([])
+            else:
+                res.append(products[left:min(left + 3, right + 1)])
+        return res
