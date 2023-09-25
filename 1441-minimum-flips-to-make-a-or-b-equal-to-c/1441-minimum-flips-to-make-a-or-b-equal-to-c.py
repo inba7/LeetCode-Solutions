20924fec639a4cd39074e6103ec57cfad10011cd
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-25 19:24:05 +0530

    Time: 12 ms (79.65%), Space: 13.2 MB (80.53%) - LeetHub

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minFlips(self, a, b, c):
+        ab, equal, ans = a | b, (a | b) ^ c, 0
+        for i in range(31):
+            mask = 1 << i
+            if equal & mask > 0:
+                if (a & mask) == (b & mask) and (c & mask) == 0:
+                    ans += 2 
+                else:
+                    ans += 1
+        return ans
