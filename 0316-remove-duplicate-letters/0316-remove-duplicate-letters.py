c2472215838daabac1f6d66f8eeece367a37f063
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-26 18:32:36 +0530

    Time: 12 ms (95.52%), Space: 13.3 MB (78.61%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def removeDuplicateLetters(self, s):
-        Check, N = {}, len(s)
-        for x in range(N):
-            Check[s[x]] = x
+        Last = {Val:Idx for Idx, Val in enumerate(s)}
 
-        Stack, Seen = [], set()
-        for x in range(N):
-            if s[x] not in Seen:
-                while Stack and Stack[-1] > s[x] and Check[Stack[-1]] > x:
-                    Seen.remove(Stack.pop())
-                Stack.append(s[x])
-                Seen.add(s[x])
-
+        Stack, Visited = [], set()
+        for Idx, Val in enumerate(s):
+            if Val not in Visited:
+                while Stack and Stack[-1] > Val and Last[Stack[-1]] > Idx:
+                    Visited.remove(Stack.pop())
+                Stack.append(Val)
+                Visited.add(Val)
+        
         return "".join(Stack)
