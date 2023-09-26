9f86bbef43def6753bad176b8ae47f909e5a2cb9
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-26 18:32:21 +0530

    Time: 22 ms (16.28%), Space: 13.4 MB (18.6%) - LeetHub

--- 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.py
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.py
@@ -3,11 +3,11 @@ class Solution(object):
         Last = {Val:Idx for Idx, Val in enumerate(s)}
 
         Stack, Visited = [], set()
-        for x in range(len(s)):
-            if s[x] not in Visited:
-                while Stack and Stack[-1] > s[x] and Last[Stack[-1]] > x:
+        for Idx, Val in enumerate(s):
+            if Val not in Visited:
+                while Stack and Stack[-1] > Val and Last[Stack[-1]] > Idx:
                     Visited.remove(Stack.pop())
-                Stack.append(s[x])
-                Visited.add(s[x])
+                Stack.append(Val)
+                Visited.add(Val)
         
-        return "".join(Stack) 
+        return "".join(Stack)
