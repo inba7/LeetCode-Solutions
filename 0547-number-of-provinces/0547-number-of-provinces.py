018ac149953f3419db80d0746427b79479d4f050
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-26 23:00:18 +0530

    Time: 161 ms (97.48%), Space: 13.9 MB (65.55%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,16 +1,19 @@
 class Solution(object):
+    def travel(self, city, mp, visited):
+        n = len(mp)
+        for next_city in range(n):
+            if next_city not in visited and mp[city][next_city] == 1:
+                visited.add(next_city)
+                self.travel(next_city, mp, visited)
+
     def findCircleNum(self, isConnected):
-        def dfs(start):
-            visited.add(start)
-            for end in range(len(isConnected)):
-                if isConnected[start][end] and end not in visited:
-                    dfs(end)
-                    
-        numOfProvinces=0
-        visited=set()
-        for start in range(len(isConnected)):
-            if start not in visited:
-                numOfProvinces+=1
-                dfs(start)
-                
-        return numOfProvinces
+        n = len(isConnected)
+
+        visited = set([])
+        group = 0
+        for city in range(n):
+            if not (city in visited):
+                visited.add(city)
+                group += 1
+                self.travel(city, isConnected, visited)
+        return group
