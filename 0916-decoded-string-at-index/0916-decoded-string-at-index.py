832322d073498739c20a341aff8fe720514be3dc
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-27 19:42:52 +0530

    Time: 16 ms (50%), Space: 13.2 MB (100%) - LeetHub

--- 0916-decoded-string-at-index/0916-decoded-string-at-index.py
+++ 0916-decoded-string-at-index/0916-decoded-string-at-index.py
@@ -1,20 +1,14 @@
 class Solution(object):
-    def decodeAtIndex(self, s, k):
-        decoded_length = 0  
-
-        for char in s:
-            if char.isdigit():
-                decoded_length *= int(char)
+    def decodeAtIndex(self, S, K):
+        A = [1]
+        for x in S[1:]:
+            if A[-1] >= K : 
+                break
+            if x.isdigit():
+                A.append( A[-1]*int(x) )
             else:
-                decoded_length += 1
-
-        for i in range(len(s) - 1, -1, -1):
-            current_char = s[i]
-            if current_char.isdigit():
-                decoded_length //= int(current_char)
-                k %= decoded_length
-            else:
-                if k == 0 or decoded_length == k:
-                    return current_char
-                decoded_length -= 1
-        return ""
+                A.append( A[-1]+1 )
+        for i in reversed(range(len(A))):
+            K %= A[i]
+            if not K and S[i].isalpha():
+                return S[i]
