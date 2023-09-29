1b98aaba1196a362b18f545252b75e65fdfe8303
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-09-29 14:30:47 +0530

    Time: 61 ms (100%), Space: 15.8 MB (59.92%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,32 +1,35 @@
 class Solution(object):
-    def canCross(self, stones):
-        m = {} 
-        n = len(stones)
-        dp = [[-1] * n for _ in range(n)]
-
-        def solve(i, k):
-            if i == n - 1:
-                return True
-
-            if dp[i][k] != -1:
-                return dp[i][k] == 1
-
-            k0, kp, k1 = False, False, False
-
-            if stones[i] + k in m:
-                k0 = solve(m[stones[i] + k], k)
-            if k > 1 and stones[i] + k - 1 in m:
-                kp = solve(m[stones[i] + k - 1], k - 1)
-            if stones[i] + k + 1 in m:
-                k1 = solve(m[stones[i] + k + 1], k + 1)
-
-            dp[i][k] = 1 if k0 or kp or k1 else 0
-            return dp[i][k] == 1
-
-        if stones[1] - stones[0] != 1:
+    def recursiveCanCross(self,stone,jump):    
+        if jump < 1:
             return False
+        if stone == self.finalStone:
+            return True
+        
+        i = self.stones[stone]
+        cacheKey = (jump*2000)+i
+        if cacheKey in self.cache:
+            return False
+        
+        if stone + jump + 1 in self.stones:
+            if self.recursiveCanCross(stone+jump+1,jump+1):
+                return True
+            
+        if stone + jump in self.stones:
+            if self.recursiveCanCross(stone+jump,jump):
+                return True
+            
+        if stone + jump - 1 in self.stones:
+            if self.recursiveCanCross(stone+jump-1,jump-1):
+                return True
+        self.cache.add(cacheKey)
+        return False
+        
+    
+    def canCross(self, stones):
+        if stones[0]+1 != stones[1]:
+            return False
+        self.stones = {stone:i for i,stone in enumerate(stones)}
+        self.finalStone = stones[-1]
+        self.cache = set()
 
-        for i in range(n):
-            m[stones[i]] = i
-
-        return solve(1, 1)
+        return self.recursiveCanCross(stones[1],1)
