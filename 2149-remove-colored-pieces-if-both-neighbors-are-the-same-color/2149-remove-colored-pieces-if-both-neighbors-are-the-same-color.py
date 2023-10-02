35b1d3405b4df5fb13af0b656e9f06b6e5e01adc
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-02 19:34:57 +0530

    Time: 87 ms (99.19%), Space: 17.3 MB (62.9%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,15 +1,5 @@
 class Solution(object):
     def winnerOfGame(self, colors):
-        Alice, Bob = 0, 0
-
-        for i in range(1, len(colors) - 1):
-            Current = colors[i]
-            Prev = colors[i-1]
-            Next = colors[i+1]
-
-            if Current == 'A' and Prev == 'A' and Next == 'A':
-                Alice += 1 
-            elif Current == 'B' and Prev == 'B' and Next == 'B':
-                Bob += 1  
-                
-        return Alice > Bob
+        a_num = sum([len(s) - 2 for s in colors.split('B') if len(s) >= 3])
+        b_num = sum([len(s) - 2 for s in colors.split('A') if len(s) >= 3])
+        return a_num > b_num
