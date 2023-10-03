b0840b279bcda47590e42a7f53cf3d40989024ad
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-03 23:34:04 +0530

    Time: 33 ms (98.84%), Space: 21.4 MB (18.32%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -1,31 +1,32 @@
 class Solution(object):
-    def deleteNode(self, root, key):
-	    if not root:
-		    return None
-
-	    if key > root.val:
-		    root.right = self.deleteNode(root.right, key)
-	    elif key < root.val:
-	    	root.left = self.deleteNode(root.left, key)
-	    else:
-		    if not root.left and not root.right:
-			    root = None
-		    elif root.right:
-			    root.val = self.successor(root)
-			    root.right = self.deleteNode(root.right, root.val)
-		    else:
-			    root.val = self.predecessor(root)
-			    root.left = self.deleteNode(root.left, root.val)
-	    return root
-
     def successor(self, root):
-	    root = root.right
-	    while root.left:
-		    root = root.left
-	    return root.val
+        root = root.right
+        while root.left:
+            root = root.left
+        return root.val
 
     def predecessor(self, root):
-	    root = root.left
-	    while root.right:
-		    root = root.right
-	    return root.val
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
+
+    def deleteNode(self, root, key):
+        if root == None:
+            return None
+
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            if (root.left == None) and (root.right == None):
+                root = None
+            elif root.right != None:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+        return root
