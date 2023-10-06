6155993961c343490ccf7bd69732ed0c383e44ac
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-06 18:06:46 +0530

    Time: 8 ms (95.45%), Space: 13.1 MB (78.28%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def integerBreak(self, n):
-        if n == 2:
-            return 1
-        if n == 3:
-            return 2
-        result = 1
-        while n > 4:
-            result *= 3
-            n -= 3
-        return result * n
+        if n == 2: return 1
+        if n == 3: return 2
+        Q = n // 3
+        R = n % 3
+        if R == 0: return 3 ** Q
+        elif R == 1: return (3 ** (Q - 1)) * 4
+        else: return (3 ** Q) * 2
