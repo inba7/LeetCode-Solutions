1eb67471fa0672abccd222d524983c48636d696c
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-09 19:37:55 +0530

    Time: 444 ms (98.92%), Space: 18.9 MB (90%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def maxProfit(self, prices, fee):
-        Pos =- prices[0]
-        Profit = 0
-        N = len(prices)
-        for i in range(1, N):
-            Pos = max(Pos, Profit-prices[i])
-            Profit = max(Profit, Pos + prices[i] - fee)
+        Profit, Pre_min = 0, prices[0]
 
+        for p in prices[1:]:
+            if p > Pre_min + fee:
+                Profit += p - Pre_min - fee
+                Pre_min = p - fee
+            else:
+                Pre_min = min(Pre_min, p)
+        
         return Profit
