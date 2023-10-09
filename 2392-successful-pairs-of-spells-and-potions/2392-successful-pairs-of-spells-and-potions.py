788b8caf47171312cf1f2fbf1dd2bc81b1e07fa1
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-10 02:32:27 +0530

    Time: 917 ms (85.83%), Space: 37.2 MB (29.43%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
@@ -1,19 +1,17 @@
 class Solution(object):
-    def successfulPairs(self, spells, potions, success):
-        potions.sort()
+    def successfulPairs(self, Spells, Potions, Success):
+        Potions.sort()
         Res = []
-        
-        for s in spells:
-            Left, Right = 0, len(potions)-1
-            Index = len(potions)
-            while Left <= Right:
-                Mid = (Left+Right)//2
-                if s * potions[Mid] >= success:
-                    Right = Mid - 1
-                    Index = Mid
-                else:
-                    Left = Mid + 1
-            
-            Res.append(len(potions)-Index)
-        
+
+        M = len(Potions)
+        maxPotion = Potions[M-1]
+
+        for Spell in Spells:
+            minPotion = (Success+Spell-1) // Spell
+            if minPotion > maxPotion:
+                Res.append(0)
+                continue
+            Index = bisect.bisect_left(Potions, minPotion)
+            Res.append(M-Index)
+
         return Res
