972641206b1e94826d636cc6d2a50cc003421622
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-10 20:04:56 +0530

    Time: 76 ms (99.65%), Space: 13.8 MB (90.59%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
@@ -1,14 +1,20 @@
-class SmallestInfiniteSet(object):
-
+class SmallestInfiniteSet:
     def __init__(self):
-        self.seen= [False] * 1005
+        self.is_present = set()
+        self.added_integers = []
+        self.current_integer = 1
 
     def popSmallest(self):
-        for i in range(1, 1005):
-            if not self.seen[i]:
-                self.seen[i] = True
-                return i
+        if len(self.added_integers):
+            answer = heapq.heappop(self.added_integers)
+            self.is_present.remove(answer)
+        else:
+            answer = self.current_integer
+            self.current_integer += 1
+        return answer
 
     def addBack(self, num):
-        if num<len(self.seen):
-            self.seen[num] = False
+        if self.current_integer <= num or num in self.is_present:
+            return
+        heapq.heappush(self.added_integers, num)
+        self.is_present.add(num)
