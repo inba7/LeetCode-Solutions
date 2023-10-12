096b831b1490565c611638f0fbae4f349f855474
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-12 14:38:13 +0530

    Time: 31 ms (76.32%), Space: 14.7 MB (39.65%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -1,19 +1,20 @@
 class Solution(object):
     def pathSum(self, root, targetSum):
-        self.count = 0
-        self.prefix_sum = {0: 1}
-        self.dfs(root, targetSum, 0)
-        return self.count
+        def DFS(node, target, Total):
+            if not node:
+                return 0
+
+            Total += node.val
+            old_total = Total - target
+            self.result += cache.get(old_total, 0)
+            cache[Total] = cache.get(Total, 0) + 1
+
+            DFS(node.left, target, Total)
+            DFS(node.right, target, Total)
+
+            cache[Total] -= 1         
         
-    def dfs(self, node, targetSum, CurrSum):
-        if not node:
-            return
-        
-        CurrSum += node.val
-        self.count += self.prefix_sum.get(CurrSum - targetSum, 0)
-        self.prefix_sum[CurrSum] = self.prefix_sum.get(CurrSum, 0) + 1
-        
-        self.dfs(node.left, targetSum, CurrSum)
-        self.dfs(node.right, targetSum, CurrSum)
-        
-        self.prefix_sum[CurrSum] -= 1
+        cache = {0:1}
+        self.result = 0
+        DFS(root, targetSum, 0)
+        return self.result
