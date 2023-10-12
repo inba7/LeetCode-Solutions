53277fbe98a46c64ff80744fb6adf8e370ed38a9
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-12 14:43:39 +0530

    Time: 60 ms (13.93%), Space: 28.3 MB (11.34%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if root in (None, p, q): return root
+        subs = [self.lowestCommonAncestor(kid, p, q) 
+                for kid in (root.left, root.right)]
+        return root if all(subs) else max(subs)
