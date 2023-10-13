b39ef9d11e0ebe5774c214aee7f8136771a704ce
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-13 17:59:23 +0530

    Time: 20 ms (21.07%), Space: 13.2 MB (86.44%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,13 +1,16 @@
 class Solution(object):
-    def rightSideView(self, root):
-        def solve(root, lvl):
-        	if root:
-        		if len(res)==lvl:
-        			res.append(root.val)
-        		solve(root.right, lvl + 1)
-        		solve(root.left, lvl + 1)
-        	return 
-
-        res = []
-        solve(root,0)
-        return res
+	def rightSideView(self, root):
+		q = [root]
+		res = []
+		while q:
+			numNodes = len(q)
+			for i in range(numNodes):
+				removeNode = q.pop(0)
+				if removeNode:
+					if removeNode.left:
+						q.append(removeNode.left)
+					if removeNode.right:
+						q.append(removeNode.right)
+					if i == numNodes - 1:
+						res.append(removeNode.val)
+		return res
