ac60ea5bb171abebe90920112903c9a8a7763119
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-16 15:53:21 +0530

    Time: 8 ms (93.44%), Space: 13.1 MB (71.33%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def getRow(self, rowIndex):
         Output = [1]
-        for _ in range(rowIndex):
-            Output.append(1)
-            for X in range(len(Output)-2, 0, -1):
-                Output[X] += Output[X-1]
+        for X in range(rowIndex):
+            Output.append(Output[X]*(rowIndex-X)//(X+1))
         return Output
