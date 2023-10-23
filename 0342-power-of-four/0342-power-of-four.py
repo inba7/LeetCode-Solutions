a418d1d3f8c9b38ff5a6760cab718af17ca16bb8
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-23 17:49:03 +0530

    Time: 18 ms (33.96%), Space: 13.2 MB (67.17%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,7 +1,5 @@
 class Solution(object):
     def isPowerOfFour(self, n):
-        if n == 1: return True
         if n <= 0: return False
-        
-        LogBase4 = math.log(n) / math.log(4)
-        return (LogBase4 == int(LogBase4))
+        LogBase4 = math.log(n, 4)
+        return LogBase4 == int(LogBase4)
