56f2957740fec6ddfbb8be955541f33a23fbc22e
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-24 08:19:30 +0530

    Time: 33 ms (15.71%), Space: 17.7 MB (71.73%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -1,16 +1,21 @@
+from collections import deque
+
 class Solution:
     def largestValues(self, root):
-        self.Max = {}
-        self.Find(root, Row=0)
-        return list(self.Max.values())
-    
-    def Find(self, root, Row):
-        if root == None:
-            return
-        if self.Max.get(Row, None) != None:
-            self.Max[Row] = max(self.Max[Row], root.val)
-        else:
-            self.Max[Row] = root.val
-
-        self.Find(root.left, Row+1)
-        self.Find(root.right, Row+1)
+        if not root:
+            return []
+        Queue = deque([(root, 0)])
+        Dict = {0:root.val}
+        
+        while Queue:
+            Curr, Row = Queue.popleft()
+            for Child in [Curr.left, Curr.right]:
+                if Child:
+                    Queue.append([Child, Row+1])
+                    if Row+1 not in Dict: Dict[Row+1] = Child.val
+                    else: Dict[Row+1] = max(Child.val, Dict[Row+1])
+        
+        Res = []
+        for Key in range(len(Dict.keys())):
+            Res.append(Dict[Key])
+        return Res
