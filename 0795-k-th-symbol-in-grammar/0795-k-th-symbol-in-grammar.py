b89f43c5e80cfcdbe65945f084c5438957995f45
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-25 11:24:56 +0530

    Time: 24 ms (6.43%), Space: 13.2 MB (73.57%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -2,7 +2,7 @@ class Solution(object):
     s = [1,0]
     def kthGrammar(self, n, k):
         if k < 2: return self.s[k]
-        for i in range(1,n):
+        for i in range(1, n):
             if k <= 2**i:
-                if i%2 == 0: return self.kthGrammar(n,2**i-k+1)
-                else: return (self.kthGrammar(n,2**i-k+1)+1) % 2
+                if i%2 == 0: return self.kthGrammar(n, 2**i-k+1)
+                else: return (self.kthGrammar(n, 2**i-k+1)+1) % 2
