3d318c895407420357f33af10c09389f80a00954
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-10-29 01:27:07 +0530

    Time: 29 ms (81.82%), Space: 13.5 MB (63.64%) - LeetHub

--- 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.py
+++ 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.py
@@ -1,10 +1,29 @@
+from string import ascii_lowercase
 class Solution(object):
+    results = {}
+    score = []
     def maxScoreWords(self, words, letters, score):
-        def BT(i, counts):
-            if i == len(words): return 0
-            word = Counter(words[i])
-            if word == word & counts:
-                curr = sum([score[ord(c)-ord("a")] * word[c] for c in word])
-                return max(curr+BT(i+1, counts-word), BT(i+1, counts))
-            return BT(i + 1, counts)
-        return BT(0, Counter(letters))
+        self.results.clear()
+        self.score = score
+        letter_count = {}
+        for index, char in enumerate(ascii_lowercase):
+            letter_count[index] = letters.count(char)
+        
+        return self.max_score(words, letter_count)
+
+    def max_score(self, words, letter_count):
+        if len(words) == 0:
+            return 0
+        curr_score = 0
+        new_letter_count = letter_count.copy()
+        for char in words[0]:
+            new_letter_count[ascii_lowercase.index(char)] -= 1
+            if new_letter_count[ascii_lowercase.index(char)] < 0:
+                curr_score = -1
+                break
+            else:
+                curr_score += self.score[ascii_lowercase.index(char)]
+        if curr_score == -1:
+            return self.max_score(words[1:], letter_count)
+        else:
+            return max(curr_score + self.max_score(words[1:], new_letter_count),  self.max_score(words[1:], letter_count))
