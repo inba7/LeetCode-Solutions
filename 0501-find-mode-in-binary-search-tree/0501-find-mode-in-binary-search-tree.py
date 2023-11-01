6b8506a06afe6ef192cde1341bd3c4d20968c070
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-11-01 23:12:07 +0530

    Time: 42 ms (25.27%), Space: 21.6 MB (5.38%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -1,22 +1,10 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def findMode(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
         maxv = {'max':0}
         hashmap = {}
         def dfs(root, parent, freq):
             if root == None:
                 return
-            
-            
             if root.val not in hashmap:
                 hashmap[root.val] = 0
             hashmap[root.val] += 1
