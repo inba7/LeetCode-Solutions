c9b01c541713f146c62c6533886b4cc5be6b6e9c
Inbaarasu T U <130481896+inba7@users.noreply.github.com>
2023-11-04 12:37:06 +0530

    Time: 119 ms (8.33%), Space: 14.2 MB (33.33%) - LeetHub

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.py
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def getLastMoment(self, n, left, right):
-        time = 0
-
-        for pos in left:
-            time = max(time, pos)
-        for pos in right:
-            time = max(time, n - pos)
-
-        return time
+        left.append(0)
+        right.append(n)        
+        
+        return max(max(left), n - min(right))
